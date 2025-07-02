d840bc4608d32161f83b4890b02d394ea2c3d54f
Roger Lozano <lozanofierroruu@gmail.com>
2025-07-02 18:49:45 +0200

    I coudl 'finish' the script but i have a 'rang to improve'. The camera follows the player, but give hits like plauer too and that's a thing to improve. By the other way, i put an 'invoke' to reactivate the anamtor and let 'play' again. I only need to destroy the object, re instantiate the object and put a great sound and maybe particles.

--- Assets/Scripts/Object/CameraRagdollControl.cs
+++ Assets/Scripts/Object/CameraRagdollControl.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Cinemachine;
+using Assets.Scripts.Core;
 
 namespace Assets.Scripts.Object
 {
@@ -23,19 +24,37 @@ namespace Assets.Scripts.Object
             _virtualCamera.Follow = _targetInRagdoll;
             _virtualCamera.LookAt = _targetInRagdoll;
 
+            Invoke("ResetCameraPositon", 4f);
+            Invoke("SwitchToPlayer", 5f);
+        }
+
+        public void SwitchToPlayer()
+        {
+            PlayerController player = FindFirstObjectByType<PlayerController>();
+
+            player.transform.position = _targetInRagdoll.position;
+            player.transform.rotation = Quaternion.identity;
+
+
+
+            player.GetComponent<Animator>().enabled = true;
+
+            _virtualCamera.Follow = _targetPlayer;
+            _virtualCamera.LookAt = _targetPlayer;
+
+        }
+
+        private void ResetCameraPositon()
+        {
+            //The idea is move in axis Y the camera, because sometiees the camera is down of ground position looking the player
             if (_virtualCamera.transform.position.y <= 2)
                 _virtualCamera.transform.position = new Vector3(
                     _virtualCamera.transform.position.x,
                     _targetInRagdoll.position.y + _highCamera,
                     _virtualCamera.transform.position.z
                 );
-        }
 
-        public void SwitchToPlayer()
-        {
-            _virtualCamera.Follow = _targetPlayer;
-            _virtualCamera.LookAt = _targetPlayer;
         }
-
     }
+
 }
